3093b62607b27b0e993fd7db5950cda6e8c94b2c
Luis Oliverez <louoliverez901@gmail.com>
2023-05-14 15:16:10 -0700

    comleted 13B

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,18 +11,16 @@ public class HelloWorld {
 
         float myNumber = 3.14f;
         System.out.println(myNumber);
-//        assign double or float with F(literal value) to indicate Float
-
-
-
-
-
-//        int x = 4;
-//        x = x + 5;
-
-        int x = 4;
-        x += 5;
-        System.out.println(x);
+/*
+        assign double or float with F(literal value) to indicate Float
+        int x = 3;
+        int y = 4;
+        y = y * x;
+*/
+        int x = 3;
+        int y = 4;
+    y *= x;
+        System.out.println(y);
 
 
 
