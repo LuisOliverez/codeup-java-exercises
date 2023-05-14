521abb0d5cf645fe1ad62a7df6a97ba5f2577f0b
Luis Oliverez <louoliverez901@gmail.com>
2023-05-14 14:01:29 -0700

    completed todo #3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,10 +2,17 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
 
+       int MyFavNum = 22;
+       System.out.println(MyFavNum);
 
-        javac HelloWorld.java
 
+       String MyString = "This is my string,";
+        System.out.println(MyString);
 
+        char MyChar = 'a';
+        MyString = MyString + MyChar;
+        System.out.println(MyString);
 
-    }
-}
+            }
+        }
+
