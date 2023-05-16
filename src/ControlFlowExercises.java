934efc64c2009f678316535275d3d983ff90ef1a
Luis Oliverez <louoliverez901@gmail.com>
2023-05-16 12:39:31 -0700

    completed #4

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,4 @@
+//import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class ControlFlowExercises {
@@ -31,7 +32,6 @@ public class ControlFlowExercises {
 //        }
 
 
-
 // -------------------------#2---------------------
 
 //        for(int i = 1; i <= 100; i++){
@@ -51,22 +51,51 @@ public class ControlFlowExercises {
 // -------------------------#3---------------------
 
 
-        Scanner sc = new Scanner(System.in);//prompt user for number
-        System.out.println("what number would you like to go up to?");
-        int limit = sc.nextInt();
-
-
-        System.out.println("Number\tSquare\tCube");//table header
-            for (int i = 1; i <= limit; i++){
-                int square = i * i;//calculate squared
-                int cube = i * i * i;//calculate cubed
-
-                System.out.printf("%-6d | %-7d | %-5d\n",i,square,cube);//display the values in tabular format
-            }
+//        Scanner sc = new Scanner(System.in);//prompt user for number
+//        System.out.println("what number would you like to go up to?");
+//        int limit = sc.nextInt();
+//
+//
+//        System.out.println("Number\tSquare\tCube");//table header
+//            for (int i = 1; i <= limit; i++){
+//                int square = i * i;//calculate squared
+//                int cube = i * i * i;//calculate cubed
+//
+//                System.out.printf("%-6d | %-7d | %-5d\n",i,square,cube);//display the values in tabular format
+//            }
 
 
 // -------------------------#4---------------------
 
+        Scanner sc = new Scanner(System.in);
+        String choice = "";
+
+        do {
+            System.out.println("Please enter a numerical grade from 1-100");
+            int numGrade = sc.nextInt();
+            sc.nextLine();
+            String letterGrade;
+            if (numGrade >= 88) {
+                letterGrade = "A";
+            } else if (numGrade >= 80) {
+                letterGrade = "B";
+            } else if (numGrade >= 67) {
+                letterGrade = "C";
+            } else if (numGrade >= 59) {
+                letterGrade = "D";
+            } else {
+                letterGrade = "F";
+            }
+
+            System.out.println("Letter grade is: " + letterGrade);
+
+            System.out.print("Would you like to continue? (yes/no): ");
+            choice = sc.nextLine();
+        } while (choice.equalsIgnoreCase("yes"));
+
+        System.out.println("Goodbye!");
+
+        sc.close();
 
     }
 
