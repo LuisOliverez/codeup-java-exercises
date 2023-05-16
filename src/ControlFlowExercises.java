3911c4f7a841b59331e1ff2414e095af350711ad
Luis Oliverez <louoliverez901@gmail.com>
2023-05-16 11:05:56 -0700

    completed all #1

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,5 @@
 public class ControlFlowExercises {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 // -------------------------#1---------------------
 //        int i = 5;
@@ -8,29 +8,35 @@ public class ControlFlowExercises {
 //            i++;
 //        }
 
-        long i = 2;
-        do {
-            System.out.println(i);
-            i *= i;
-        }while (i < 1000000L);
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        }while (i < 1000000L);
+
 //if int has capacity, why is a long needed to prevent overflow???
+
+
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i < 1000000L);
+
+
+        for (long i = 2; i <1000000L; i *= i){
+            System.out.println(i);
+        }
 
 
 
 // -------------------------#2---------------------
 
 
-
 // -------------------------#3---------------------
 
 
-
 // -------------------------#4---------------------
-
-
-
-
-
 
 
     }
