99ff4e1ad4765c7d67524bd0f2e5ca8e107aaf21
Luis Oliverez <louoliverez901@gmail.com>
2023-05-16 09:35:34 -0700

    completed 1B-A

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,33 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
 
-        int i = 5;
-        while(i <= 15) {
-            System.out.print(i);
-            i++;
-        }
+// -------------------------#1---------------------
+//        int i = 5;
+//        while(i <= 15) {
+//            System.out.print(i);
+//            i++;
+//        }
+
+        int i = 0;
+        do {
+            System.out.println(i);
+            i += 2;
+        }while(i <= 100);
+
+
+
+// -------------------------#2---------------------
+
+
+
+// -------------------------#3---------------------
+
+
+
+// -------------------------#4---------------------
+
+
+
 
 
 
