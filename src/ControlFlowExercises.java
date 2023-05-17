86b05a723409468777acaed619380035aa89bbb4
Luis Oliverez <louoliverez901@gmail.com>
2023-05-17 08:17:36 -0700

    uncommented solutions

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,85 +5,85 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
 // -------------------------#1---------------------
-//        int i = 5;
-//        while(i <= 15) {
-//            System.out.print(i + " ");
-//            i++;
-//        }
+        int i = 5;
+        while(i <= 15) {
+            System.out.print(i + " ");
+            i++;
+        }
 
-//        long i = 0;
-//        do {
-//            System.out.println(i);
-//            i += 2;
-//        }while (i <= 100);
-
-
-//        int i = 100;
-//        do {
-//            System.out.println(i);
-//            i-=5;
-//        }while (i >= -10);
+        long i = 0;
+        do {
+            System.out.println(i);
+            i += 2;
+        }while (i <= 100);
 
 
-
-//        long i = 2;
-//        do {
-//            System.out.println(i);
-//            i *= i;
-//        } while (i < 1000000L);
-
-//
-//        for (long i =100; i >= -10; i -= 5){
-//            System.out.println(i);
-//        }
+        int i = 100;
+        do {
+            System.out.println(i);
+            i-=5;
+        }while (i >= -10);
 
 
-//        for (long i = 2; i <1000000L; i *= i){
-//            System.out.println(i);
-//        }
+
+        long i = 2;
+        do {
+            System.out.println(i);
+            i *= i;
+        } while (i < 1000000L);
+
+
+        for (long i =100; i >= -10; i -= 5){
+            System.out.println(i);
+        }
+
+
+        for (long i = 2; i <1000000L; i *= i){
+            System.out.println(i);
+        }
 
 //
 //// -------------------------#2---------------------
 //
-//        for(int i = 1; i <= 100; i++){
-//            if(i % 3 == 0 && i % 5 ==0){
-//                System.out.println("FizzBuzz");
-//            }else if(i %3 == 0){
-//                System.out.println("Fiz");
-//            } else if (i % 5 ==0) {
-//                System.out.println("Buzz");
-//            }else {
-//                System.out.println(i);
-//            }
-//        }
-//
+        for(int i = 1; i <= 100; i++){
+            if(i % 3 == 0 && i % 5 ==0){
+                System.out.println("FizzBuzz");
+            }else if(i %3 == 0){
+                System.out.println("Fiz");
+            } else if (i % 5 ==0) {
+                System.out.println("Buzz");
+            }else {
+                System.out.println(i);
+            }
+        }
+
 
 //
 //// -------------------------#3---------------------
 //
 //
 
-//        boolean confirm = true;
-//
-//        do {
-//            Scanner sc = new Scanner(System.in);//prompt user for number
-//            System.out.println("what number would you like to go up to?");
-//            int userInput = sc.nextInt();
-//
-//            System.out.println("Here is your table!");
-//            System.out.println("Number\tSquared\tCubed");//table header
-//            System.out.println("------ | ------- | -----");
-//            for (int i = 1; i <= userInput; i++) {
-//                int square = i * i;//calculate squared
-//                int cube = i * i * i;//calculate cubed
-//
-//                System.out.printf("%-6d | %-7d | %-5d\n", i, square, cube);//display the values in tabular format
-//            }
-//            System.out.println("Continue?(y/n)");
-//            confirm = sc.next().equalsIgnoreCase("y");
-//        }while(confirm);
-//        System.out.println("Have a good day!");
-//
+        boolean confirm = true;
+
+        do {
+            Scanner sc = new Scanner(System.in);//prompt user for number
+            System.out.println("what number would you like to go up to?");
+            int userInput = sc.nextInt();
+
+            System.out.println("Here is your table!");
+            System.out.println("Number\tSquared\tCubed");//table header
+            System.out.println("------ | ------- | -----");
+            for (int i = 1; i <= userInput; i++) {
+                int square = i * i;//calculate squared
+                int cube = i * i * i;//calculate cubed
+
+                System.out.printf("%-6d | %-7d | %-5d\n", i, square, cube);//display the values in tabular format
+            }
+            System.out.println("Continue?(y/n)");
+            confirm = sc.next().equalsIgnoreCase("y");
+        }while(confirm);
+        System.out.println("Have a good day!");
+
 //// -------------------------#4---------------------
 //
         Scanner sc = new Scanner(System.in);
