c983f8fb666c9f7d304f359c189bd294d396895f
Luis Oliverez <louoliverez901@gmail.com>
2023-05-17 10:21:45 -0700

    starting exe

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,6 +1,10 @@
 public class StringExercise {
     public static void main(String[] args){
 
+        String education = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(education);
+
     }
 
 }
