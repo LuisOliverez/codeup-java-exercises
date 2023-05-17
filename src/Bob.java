9e9cb890627836953ccb9802c1c8349031138005
Luis Oliverez <louoliverez901@gmail.com>
2023-05-17 14:41:00 -0700

    put Bob exercise in do while loop

--- src/Bob.java
+++ src/Bob.java
@@ -4,20 +4,28 @@ public class Bob {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Do you have a question or comment for Bob?");
+        System.out.println("This is Bob.");
+        String userAnswer;
 
-            String userInput = sc.nextLine();
+        do {
+            System.out.println("Get Bobs attention, please.");
+
+            String userInput = sc.nextLine().trim();
             String response;
-            if (userInput.endsWith("?")) {
-                response = "Sure";
+
+            if (userInput.equals("")) {
+                response = "Fine. Be that way!";
             } else if (userInput.endsWith("!")) {
                 response = "Whoa, chill out!";
-            } else if (userInput.equals("")){
-                response = "Fine. Be that way!";
+            } else if (userInput.endsWith("?")){
+                response = "Sure";
             }else {
                 response = "Whatever";
             }
 
-            System.out.println("Bob: " + response);
-        }
+            System.out.println("Bob said: " + response);
+            System.out.println("Would you like to continue(y/n)");
+                userAnswer = sc.nextLine();
+        }while (userAnswer.equalsIgnoreCase("y"));
     }
+}
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,9 +1,8 @@
 public class StringExercise {
     public static void main(String[] args){
 
-        String education = "We don't need no education\n" +
-                "We don't need no thought control";
-        System.out.println(education);
+        String education = "We don't need no education";
+        System.out.println(education.replace("education", "thought control"));
 
 
         String checkThisOut = "Check \"this\" out!, \"s inside of \"s!\n ";
@@ -12,8 +11,7 @@ public class StringExercise {
         String windows = "In windows, the main drive is usually C:\\\n";
         System.out.println(windows);
 
-        String backSlash = "I can do backslashes \\, double backslashes \\\\,\n" +
-                "and the amazing triple backslash \\\\\\!\n";
+        String backSlash = "I can do backslashes \\, double backslashes \\\\, \n and the amazing triple backslash \\\\\\!\n";
         System.out.println(backSlash);
     }
 
