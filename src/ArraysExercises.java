282618377343972ad793ed70633d9d48f009782f
Luis Oliverez <louoliverez901@gmail.com>
2023-05-23 11:33:28 -0700

    completed Arrays Exercise #1

---
+++ src/ArraysExercises.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main(String[] args) {
+
+        int [] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+
+
+        String [] peopleArr = {"Tim", "Jim", "Kim"};
+        String personToAdd = "Bob";
+
+        String[] updatedPeople = new String[peopleArr.length + 1];
+        for (int i= 0 ; i< peopleArr.length ; i++) {
+            updatedPeople[i]= peopleArr[i];
+        }
+        updatedPeople[updatedPeople.length -1]=personToAdd;
+
+        for (String people : updatedPeople) {
+            System.out.println(people);
+        }
+
+    }
+}
