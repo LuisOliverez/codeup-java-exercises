53c4f655f380131aadf28b36da325f2b93199a83
Luis Oliverez <louoliverez901@gmail.com>
2023-05-25 12:00:08 -0700

    Started Interfaces and Abstract Exercise. Starting #4

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -3,29 +3,6 @@ package shapes;
 public class Rectangle {
 
 
-    public final double length;
-    public final double  width;
-
-
-    //constructor
-    public Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
-    }
-
-
-    //calculate Area method
-    public double getArea(){
-        System.out.println("Area from Rectangle Class is: " + length * width);
-        return length * width;
-    }
-
-    //calculate Perimeter method
-    public  double getPerimeter(){
-        System.out.println("Perimeter from Rectangle class is: " + 2 * (width * width));
-        return 2 * (length + width);
-    }
-
 
 
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,27 +1,9 @@
 package shapes;
 
     public class Square extends Rectangle {
-        private double side;
-
-
-        //constructor
-        public Square (double side) {
-            super(side, side);
-            this.side = side;
-
-    }
 
 
 
-        @Override
-        public double getArea() {
-            System.out.println("Area from Square Class");
-            return side*side;
-        }
 
-        @Override
-        public double getPerimeter() {
-            System.out.println("Perimeter from Square Class");
-            return 4*side;
-        }
+
     }
