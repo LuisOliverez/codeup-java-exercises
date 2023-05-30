9f5d2b9c414622fdf12e9d2ea8cc277a9cee851b
Luis Oliverez <louoliverez901@gmail.com>
2023-05-30 12:24:37 -0700

    completed Collections Exercise #1

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,41 @@
 package grades;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+    //constructor for name and initializes grades as ArrayList
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    //Name setter(getName method)
+    public String getName() {
+        return name;
+    }
+
+    //method to add grade to ArrayList using (.add) and passing grade through it
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    //getGradeAverage method
+    public double getGradeAverage(){
+        if (grades.isEmpty()){
+            return 0.0;//if no grades return 0
+        }else {//otherwise calculate grade
+            int sum = 0;
+            for (int grade : grades){
+                sum += grade;
+            }
+            return (double) sum / grades.size();
+        }
+    }
+
+
+
+
 }
--- src/grades/StudentTest.java
+++ src/grades/StudentTest.java
@@ -3,10 +3,24 @@ package grades;
 import java.util.ArrayList;
 
 public class StudentTest {
-    private String name;
-    private ArrayList<Integer> grades;
+    public static void main(String[] args) {
+        Student student1 = new Student("John Jacob");
+        Student student2 = new Student("Pawl Rubb");
+
+        student1.addGrade(80);
+        student1.addGrade(87);
+        student1.addGrade(76);
+        student1.addGrade(93);
 
 
+        student2.addGrade(78);
+        student2.addGrade(75);
+        student2.addGrade(68);
+        student2.addGrade(89);
 
+
+        System.out.println(student1.getName()+ "'s average grade is: " +student1.getGradeAverage());
+        System.out.println(student2.getName()+ "'s average grade is: " +student2.getGradeAverage());
+    }
 
 }
